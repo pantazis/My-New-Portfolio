e878916b81d466cadde801206792f83af910e2c3
pantazis <pantazisvastardis@yahoo.com>
2015-11-26 16:41:17 +0200

    nav dynamic

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 // JavaScript Document
 
-   $(document).ready(function () {
-            $(".main").onepage_scroll({
-                sectionContainer: "section",
-                responsiveFallback: 600,
-                loop: true
-            });
-        });
+$(document).ready(function () {
+    $(".main").onepage_scroll({
+        sectionContainer: "section",
+        responsiveFallback: 600,
+        loop: true
+    });
+});
+
+
+//add class to menu
+
+//if()
+function addClass (i) {
+    var trg = i.target.parentElement;
+  
+    if(trg.nodeName==="LI"){trg.classList.add("active");}
+    
+    
+    
+
+    
+}
+
+
+
+
+
+var nav1 = document.getElementById("nav1")
+
+nav1.addEventListener("click", addClass,false );
