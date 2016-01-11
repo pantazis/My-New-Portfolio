4baa097aeb61be6c95df61732dbcdc6858e30edf
pantazis <pantazisvastardis@yahoo.com>
2016-01-11 10:46:57 +0200

    fixed about Us

---
+++ js/script3.js
@@ -0,0 +1,13 @@
+var text1 = document.getElementById("text1").innerHTML
+var text2 = document.getElementById("text2").innerHTML
+var text3 = document.getElementById("text3").innerHTML
+
+
+document.getElementById("item1").innerHTML = text1;
+document.getElementById("item2").innerHTML = text2;
+document.getElementById("item3").innerHTML = text3;
+
+console.log(document.getElementById("item1"));
+console.log(document.getElementById("item2"));
+console.log(document.getElementById("item3"));
+
