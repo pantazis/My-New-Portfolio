f3504278e0cbb5475067855a5e52f7d47a1d3813
pantazis <pantazisvastardis@yahoo.com>
2016-01-18 11:43:48 +0200

    front page save

--- js/script3.js
+++ js/script3.js
@@ -7,18 +7,83 @@ document.getElementById("item1").innerHTML = text1;
 document.getElementById("item2").innerHTML = text2;
 document.getElementById("item3").innerHTML = text3;
 
-console.log(document.getElementById("item1"));
-console.log(document.getElementById("item2"));
-console.log(document.getElementById("item3"));
+
 var page1 = document.querySelector(".page1");
 
-function resizepage1 (){
+
+function resizepage1() {
 
 
- page1.style.width = window.innerWidth+"px";
- page1.style.height = window.innerHeight+"px";
+    page1.style.width = window.innerWidth + "px";
+    page1.style.height = window.innerHeight + "px";
 };
 
-//resizepage1();
-//window.onresize = function(){ resizepage1();};
+
+var effect2 = document.querySelector(".effect2 ");
+resizepage1();
+window.onresize = function () {
+    var effect2 = document.querySelector(".effect2 ");
+    resizepage1();
+};
+
+
+
+document.querySelector(".page1").addEventListener("mousemove", function (event) {
+    myFunction(event);
+});
+document.querySelector(".page1").addEventListener("load", function (event) {
+    myFunction(event);
+});
+
+function myFunction(e) {
+
+
+    var x = e.clientX;
+    var y = e.clientY;
+
+    var screenw =  window.innerWidth;
+    var screenh = window.innerHeight;
+
+    var muvex = (x / screenw) *100/10;
+    var muvey = (y / screenh) * 100/10;
+    
+    effect2.style.left = "-"+10 - muvex+ "%";
+    effect2.style.top = "-"+10 - muvey+ "%";
+   
+      
+
+
+
+
+}
+
+
+var box1 = document.querySelector("#div1");
+ 
+box1.onmouseover= function(){
+    document.querySelector("#div2").className ="box2 hover";
+   
+    
+};
+var box2 = document.querySelector("#div3");
+box2.onmouseover= function(){
+    document.querySelector("#div4").className ="box2 hover";
+    
+};
+
+
+function clickbbb(){
+	
+	var ab = document.querySelector(".one").childNodes[1].childNodes[0];
+ab.click()
+};
+
+
+
+	
+	
+	
+	
+
+
 
