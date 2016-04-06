215576e2355c6bf99286fda81a84be4d7eead787
pantazis <pantazisvastardis@yahoo.com>
2016-04-06 07:43:55 +0300

    about us

--- js/script3.js
+++ js/script3.js
@@ -1,11 +1,4 @@
-var text1 = document.getElementById("text1").innerHTML
-var text2 = document.getElementById("text2").innerHTML
-var text3 = document.getElementById("text3").innerHTML
 
-
-document.getElementById("item1").innerHTML = text1;
-document.getElementById("item2").innerHTML = text2;
-document.getElementById("item3").innerHTML = text3;
 
 
 var page1 = document.querySelector(".page1");
@@ -94,6 +87,96 @@ function clickb(){
 	var ab = document.querySelector(".one").childNodes[2].childNodes[0];
 ab.click()
 };
+
+function about (){
+var containerfrontW = window.innerWidth /1.1612+"px" ;
+var containerfrontH = window.innerHeight /1.4358+"px" ;
+document.getElementById("about").style.width = containerfrontW;
+document.getElementById("about").style.height = containerfrontH;
+}
+
+
+
+function h2p (i){
+	var targeta = i.target.id;
+	var text = i.target.parentNode.children[1];
+	var artcle = document.getElementsByClassName("artcle");
+	var artcleh1 = artcle[0].children[0].children[1]
+	var artcleh2 = artcle[1].children[0].children[1]
+	var artcleh3 = artcle[2].children[0].children[1]
+	
+	
+	console.log(artcle[0].children[0].children[1]);
+	console.log(i.target.parentNode);
+	
+		switch(i.target.id) {
+		
+    case "a":
+		clickb();
+
+       
+        break;
+    case "b":
+		clickb();
+
+
+        
+        break;
+		      
+    case "c":
+	clickb();
+
+		
+        
+    break;
+	
+    
+} 
+
+	
+	
+	function clickb(){
+
+	console.log(text.className);
+	
+	if (text.className == "none" | text.className == "hidep"|text.className == "div2"){
+		artcleh1.className = "div2";
+	artcleh2.className = "div2";
+	artcleh3.className = "div2";
+		
+		
+	text.className = "div";
+
+		}else if(text.className == "div"){
+
+	
+
+
+	artcleh1.className = "div2";
+	artcleh2.className = "div2";
+	artcleh3.className = "div2";
+			text.style.display = "div2";
+	}
+		console.log(text.className);
+	
+	
+	
+}
+
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+document.getElementById("about").addEventListener("click", h2p, false);
+
+
+
 
 
 
